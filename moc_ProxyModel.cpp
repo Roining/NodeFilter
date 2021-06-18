dedf348fec17a445c62fe688b12d395478d7fea7
Flower <gataulin.ilia@gmail.com>
2021-06-19 03:35:48 +0400

    add storage

--- moc_ProxyModel.cpp
+++ moc_ProxyModel.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../NodeFilter/include/ProxyModel.h"
+#include "include/ProxyModel.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
--- moc_TreeModel.cpp
+++ moc_TreeModel.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../NodeFilter/include/TreeModel.h"
+#include "include/TreeModel.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
