f13417d0d70e262593454263bc4b9365779f9a71
Flower <gataulin.ilia@gmail.com>
2021-10-09 18:47:47 +0400

    update service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 's-app-v1.31'
-const dynamicCacheName = 'd-app-v1.31'
+const staticCacheName = 's-app-v1.32'
+const dynamicCacheName = 'd-app-v1.32'
 const assetUrls = [ '/',
 '/NodeFilter.html',
 '/index.html',
